10d2c85d80c668175eeec2367b4d2788ceeec94a
Ivan Wills <ivan.wills@gmail.com>
2015-03-08 08:55:31 +1100

    Fixed spelling

--- lib/App/filewatch.pm
+++ lib/App/filewatch.pm
@@ -23,93 +23,58 @@ __END__
 
 =head1 NAME
 
-App::filewatch - <One-line description of module's purpose>
+App::filewatch - Watch files and directories for changes
 
 =head1 VERSION
 
 This documentation refers to App::filewatch version 0.0.1
 
-
 =head1 SYNOPSIS
 
-   use App::filewatch;
+   file-watch [option] [file(s)]
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
+ OPTIONS:
+  -d --dir      Check directories
+     --no-dir   Don't check directories
+  -t --type[=]str
+                Specify the type of file events to listen for, more than
+                one event can be specified.
+                 - IN_ACCESS
+                 - IN_MODIFY
+                 - IN_ATTRIB
+                 - IN_CLOSE_WRITE
+                 - IN_CLOSE_NOWRITE
+                 - IN_OPEN
+                 - IN_ALL_EVENTS
+                 - IN_MOVED_FROM
+                 - IN_MOVED_TO
+                 - IN_CREATE
+                 - IN_DELETE
+                 - IN_DELETE_SELF
+                 - IN_MOVE_SELF
+                 - ALL - special pseudo event setting all events on so you can
+                         see which ones are actually fired
+  -r --report   When killed, a report will be generated of all files modified
 
+  -v --verbose  Show more detailed option
+     --version  Prints the version information
+     --help     Prints this help information
+     --man      Prints the full documentation for file-watch
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+C<file-watch> uses iNotify to track when files change. The types of changes
+can be found in L<Linux::Inotify2>.
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
-
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-=head3 C<new ( $search, )>
-
-Param: C<$search> - type (detail) - description
-
-Return: App::filewatch -
-
-Description:
-
-=cut
-
-
 =head1 DIAGNOSTICS
-
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
@@ -120,11 +85,10 @@ Patches are welcome.
 =head1 AUTHOR
 
 Ivan Wills - (ivan.wills@gmail.com)
-<Author name(s)>  (<contact address>)
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2015 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
+Copyright (c) 2014-2015 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
 All rights reserved.
 
 This module is free software; you can redistribute it and/or modify it under
